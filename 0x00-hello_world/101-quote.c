ed395865d8f7b20b798658a32b009a67862b3d5d
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-08 15:46:00 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
 	return (1);
-}	
+}
