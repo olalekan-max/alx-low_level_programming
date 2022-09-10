08ff5084f46582f924e71054b20d5cd84bc39351
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-10 03:30:14 -0700

    all commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabetic
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar (c);
+
+	putchar('\n');
+
+	return (0);
+}
