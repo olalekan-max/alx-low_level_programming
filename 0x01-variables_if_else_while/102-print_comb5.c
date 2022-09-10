6e446714a89871d68a82e54fe7e31cba79aad6b6
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-10 15:45:40 -0700

    my commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - prints 3 combinations of numbers
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b = 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (((k + j) > (c + i) && k >= c) || c < k)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(c);
+						putchar(d);
+
+						if (c + i + K + j == 227 && c == 57)
+						{
+						break;
+						}
+						else
+						{
+						putchar(',');
+						putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
