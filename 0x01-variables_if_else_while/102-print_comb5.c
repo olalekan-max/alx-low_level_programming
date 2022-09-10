c60ff1e0a2408ba3bcf22bbab692418ffc1a01e0
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-10 16:19:28 -0700

    my commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			{
 				for (d = 48; d <= 57; d++)
 				{
-					if (((k + j) > (c + i) && k >= c) || c < k)
+					if (((c + d) > (a + b) && c >= a) || a < c)
 					{
 						putchar(a);
 						putchar(b);
@@ -26,7 +26,7 @@ int main(void)
 						putchar(c);
 						putchar(d);
 
-					if (c + i + k + j == 227 && c == 57)
+					if (a + b + c + d == 227 && a == 57)
 					{
 					break;
 					}
