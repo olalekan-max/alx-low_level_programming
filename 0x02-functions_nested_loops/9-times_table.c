9f5d970dce0887a1b866f6b315380831b9c1bb16
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-13 16:45:49 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,8 @@ void times_table(void)
 
 	for (rone = 0; rone <= 9; rone++)
 	{
-		_putchar		_putchar(',');
+		_putchar('0');
+		_putchar(',');
 		_putchar(' ');
 		for (cone = 1; cone <= 9; cone++)
 		{
