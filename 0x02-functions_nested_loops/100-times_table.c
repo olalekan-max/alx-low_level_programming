3838a569c15af5b7a3a0e54512b9f4f64272d21c
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-14 02:35:05 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,11 @@ void print_times_table(int n)
 
 	if (!(n > 15 || n < 0))
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
+			for (j = 0; j <= n; j++)
+			{
+
 			res = (i * j);
 			if (j != 0)
 			{
