f40d824cd21a2c5667b5cc8a59e87a9bddf9e9fd
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-13 18:02:59 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
+#include "maain.h"
 #include <stdio.h>
-#include "_putchar.h"
 /**
  * print_to_98 - a function that prints all natural numbers from n to 98
  * user input's number prints to 98, regardless < 98 or > 98
