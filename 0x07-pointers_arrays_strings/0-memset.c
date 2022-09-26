3e3d7eb1c17943f69f1dc276a4a56889bdaa82ef
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-26 13:12:47 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * _memset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to the memory block
+ *
+ * Return: pointer to the memory block
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
