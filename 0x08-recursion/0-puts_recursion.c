34d8951df36ad1a905659c89ac03e0479a3fd074
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-09-28 02:55:24 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,10 +12,10 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
