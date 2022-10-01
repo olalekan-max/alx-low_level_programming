ae037dae49d64e06fcae7acd03407d9766f32246
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-10-01 12:25:33 -0700

    my commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the same name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv[0]);
+
+	return (0);
+}
