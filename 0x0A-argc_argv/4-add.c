72c6ee703e5eb3a85a0ffdfc5c5e7e02af077737
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-10-01 13:22:49 -0700

    my commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,86 @@
 #include <stdio.h>
 #include "main.h"
-#include <ctype.h>
 
 /**
- * main - adds numbers
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointer to strings
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
  *
- * Return: 0 if no errors, else 1
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			+dd;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: array of arguments
  */
 
 int main(int argc, char *argv[])
 {
-	int a = 0, i, j;
+	int sum, num, i, j, k;
+
+	sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (isdigit(argv[i][j]) == 0)
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
 				puts("Error");
 				return (1);
 			}
 		}
 	}
-	for (i = 1; i < argc; i++)
+	for (k = 1; k < argc; k++)
 	{
-		a += atoi(argv[i]);
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
 	}
-	printf("%d\n", a);
+
+	printf("%d\n", sum);
 	return (0);
 }
