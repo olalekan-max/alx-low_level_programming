15e39368c957272b9c8aa8f1cbf83d5ded2b4eac
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-10-01 12:31:35 -0700

    my commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (success)
  */
 
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
