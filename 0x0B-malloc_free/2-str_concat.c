c153d2794461d915145b7ce16556439ae3d6801f
olalekan-max <mathewoo.19@student.funaab.edu.ng>
2022-10-03 23:24:48 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,10 @@ char *str_concat(char *s1, char *s2)
 	char *concat_str;
 	int index, concat_index = 0, len = 0;
 
-	if (s1 == Null)
+	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 == Null)
+	if (s2 == NULL)
 		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
@@ -25,8 +25,8 @@ char *str_concat(char *s1, char *s2)
 
 	concat_str = malloc(sizeof(char) * len);
 
-	if (concat_str == Null)
-		return (Null);
+	if (concat_str == NULL)
+		return (NULL);
 
 	for (index = 0; s1[index]; index++)
 		concat_str[concat_index++] = s1[index];
